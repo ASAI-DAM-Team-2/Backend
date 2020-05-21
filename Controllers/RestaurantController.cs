9a9c5e77bf39f922b6ecc7c85e1cf94493e49afb
Filip Pavletic <15568085+fpavletic@users.noreply.github.com>
2020-05-21 11:14:51 +0100

    Updated all of the controllers to use the generic CrudController added in the last commit

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web.Http;
 
 namespace AllergyApp.Controllers
@@ -8,29 +9,26 @@ namespace AllergyApp.Controllers
     [Authorize]
     public class RestaurantController : CrudController<Restaurant>
     {
-        // GET api/restaurant
-        public IEnumerable<Restaurant> Get()
+        public RestaurantController() : base(EntityGetter, IdComparatorFactory, DataUpdater)
         {
-            using (var entities = new AllergyAppDb())
-            {
-                return entities.Restaurants.ToList();
-            }
         }
 
-        // GET api/restaurant/5
-        public IHttpActionResult Get(int id)
+        private static DbSet<Restaurant> EntityGetter(AllergyAppDb context)
         {
-            using (var entities = new AllergyAppDb())
-            {
-                var matchingRestaurants = entities.Restaurants.Where(r => r.restaurant_id == id);
-                if (matchingRestaurants.Count() == 1)
-                {
-                    return Ok(matchingRestaurants.First());
-                } else
-                {
-                    return BadRequest("No restaurant matches the provided Id");
-                }
-            }
+            return context.Restaurants;
+        }
+
+        private static void DataUpdater(Restaurant oldData, Restaurant newData)
+        {
+            oldData.name = newData.name;
+            oldData.adress = newData.adress;
+            oldData.company_id = newData.company_id;
+            oldData.plan_id = newData.plan_id;
+        }
+
+        private static Expression<Func<Restaurant, bool>> IdComparatorFactory(int id)
+        {
+            return (r) => r.restaurant_id == id;
         }
 
         [Route("api/Restaurant/{restaurantId:int}/Dish")]
@@ -38,65 +36,15 @@ namespace AllergyApp.Controllers
         public IHttpActionResult GetRestaurantDishes(int restaurantId)
         {
             using (var entities = new AllergyAppDb())
-            { 
-                if ( entities.Restaurants.Where(r => r.restaurant_id == restaurantId).Count() == 0)
+            {
+                if (entities.Restaurants.Where(r => r.restaurant_id == restaurantId).Count() == 0)
                 {
                     return BadRequest("No restaurant matches the provided Id");
                 }
-                
+
                 return Ok(entities.Dishes.Where(d => d.restaurant_id == restaurantId).ToList());
             }
         }
 
-        // POST api/restaurant
-        public IHttpActionResult Post([FromBody]Restaurant restaurant)
-        {
-            using ( var entities = new AllergyAppDb())
-            {
-                var restaurants = entities.Restaurants;
-                restaurant = restaurants.Add(restaurant);
-                entities.SaveChanges();
-                return Ok(restaurant);
-            }
-        }
-
-        // PUT api/restaurant/5
-        public IHttpActionResult Put(int id, [FromBody]Restaurant newData)
-        {
-            using ( var entities = new AllergyAppDb())
-            {
-                try
-                {
-                    var oldData = entities.Restaurants.First(r => r.restaurant_id == id);
-                    oldData.name = newData.name;
-                    oldData.adress = newData.adress;
-                    oldData.company_id = newData.company_id;
-                    oldData.plan_id = newData.plan_id;
-                    entities.SaveChanges();
-                    return Ok(newData);
-                } catch (InvalidOperationException e) {
-                    return BadRequest("No restaurant matches the provided Id");
-                };
-            }
-        }
-
-        // DELETE api/restaurant/5
-        public IHttpActionResult Delete(int id)
-        {
-            using (var entities = new AllergyAppDb())
-            {
-                try
-                {
-                    entities.Restaurants.Remove(entities.Restaurants.First(r => r.restaurant_id == id));
-                    entities.Dishes.RemoveRange(entities.Dishes.Where(d => d.restaurant_id == id));
-                    entities.SaveChanges();
-                    return Ok();
-                }
-                catch (InvalidOperationException e)
-                {
-                    return BadRequest("No restaurant matches the provided Id");
-                }
-            }
-        }
     }
 }
