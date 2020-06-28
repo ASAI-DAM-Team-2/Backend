a2fbed390a3294fe1a1d5808af9b526591caddd0
Filip Pavletic <15568085+fpavletic@users.noreply.github.com>
2020-06-28 22:11:38 +0200

    no message

---
+++ Migrations/202006280732433_NewDb.cs
@@ -0,0 +1,16 @@
+﻿namespace AllergyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewDb : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Migrations/202006280745433_Maybe2.cs
@@ -0,0 +1,16 @@
+﻿namespace AllergyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Maybe2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
