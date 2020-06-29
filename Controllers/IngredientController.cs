115a93f9cb55e32f4d6f015c1dc451a65f69ec6a
Filip Pavletic <15568085+fpavletic@users.noreply.github.com>
2020-06-29 19:15:17 +0200

    no message

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace AllergyApp.Controllers
+{
+    public class IngredientController : CrudController<Ingredient>
+    {
+        public IngredientController() : base(EntityGetter, IdComparatorFactory, DataUpdater)
+        {
+        }
+
+        private static DbSet<Ingredient> EntityGetter(AllergyAppDb context)
+        {
+            return context.Ingredients;
+        }
+
+        private static void DataUpdater(Ingredient oldData, Ingredient newData)
+        {
+            oldData.name = newData.name;
+            oldData.picture = newData.picture;
+        }
+
+        private static Expression<Func<Ingredient, bool>> IdComparatorFactory(int id)
+        {
+            return (a) => a.ingredient_id == id;
+        }
+    }
+}
